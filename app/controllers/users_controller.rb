00ac689847bab3d480cd19cf5485fa86cc08d738
GitSP917 <GisSP9675@gmail.com>
2018-04-27 03:24:01 +0000

    機能の実装完了_ページネイト

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,7 +42,7 @@ class UsersController < ApplicationController
   #お気に入り一覧表示用
   def likes
     @user = User.find(params[:id])
-    @liking =@user.likings
+    @liking =@user.likings.page(params[:page])
     counts(@user)
   end
 
